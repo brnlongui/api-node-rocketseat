23ac24f4a6bee7df614c29981b801341c313df68
brnlongui1 <brunorlongui@gmail.com>
2020-02-21 16:23:10 -0400

    add connection

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+mongoose.connect('mongodb://localhost/nodeapi:27017',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true, 
+    useFindAndModify: false,
+    useCreateIndex: true,
+});
+
+require('./controllers/AuthController')(app);
+
+app.listen(3000);
